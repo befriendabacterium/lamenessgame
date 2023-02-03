a4582167d25c097fa49eed5d592543d842f9f8f8
Matt Lloyd Jones <42059798+befriendabacterium@users.noreply.github.com>
2023-02-03 09:38:33 +0000

    Documentation + removing redundancy
    Added more comments to explain what code it doing and found some now-redundant bits of code that i've now removed

--- src/2_wrangledata.R
+++ src/2_wrangledata.R
@@ -8,10 +8,11 @@ library(ggplot2)
 
 #read in raw study data
 studydata_raw<-readxl::read_excel('inputs/lamenessstudydata_010721.xlsx')
-# #detach(tibble)
+#read in R data file with the desired new column names
 newcolnames<-readRDS('inputs/newcolnames.RDS')
 
 # FORMATTING: PARTICIPANT PERFORMANCE DATA --------------------------------------------------------------
+#Here we do some initial, basic formatting on the dataframe
 
 #REDUNDANT COLUMN REMOVAL (EASIER TO WORK WITH)
 #make a vector of columns to remove
@@ -35,83 +36,59 @@ apply(studydata_formatted,1,class)
 studydata_formatted<-studydata_formatted[-which(studydata_formatted$ID==36),]
 
 # ARCSINE-TRANSFORM SCORES --------------------------------------------------------------
+#Here we arcsine-transform the outcome data, which is expressed in percentages, to make it more amenable to parametric analysis
 
-#arcsin-transformed accuracy
+#create a new column of arcsin-transformed accuracy scores
 studydata_formatted$accuracy_asin<-asin(sqrt(0.01*studydata_formatted$accuracy))
 #arcsin-transformed recall
 studydata_formatted$recall_asin<-asin(sqrt(0.01*studydata_formatted$recall))
 
-# PARSE FARMING EXPERIENCE  ---------------------------------------------------------------------
+# PARSE FARMING/SHEEP FARMING EXPERIENCE  ---------------------------------------------------------------------
+# Here we do some tidying on the farming/sheep farming experience data to make it easier to work with/plot sensibly
 
-farmingexp_df<-studydata_formatted %>% select(farmingexp_YN, sheeproles_type, sheeplamenessexp_prev,sheepexp_years)
-
-#change column classes
-farmingexp_df<-farmingexp_df %>%
-  mutate(across(c(farmingexp_YN,
-                  sheeproles_type, 
-                  sheeplamenessexp_prev),
-                factor))
-
-#combine sheep roles type cos poor sampling at tails
-levels(farmingexp_df$sheeproles_type)
-
-levels(farmingexp_df$sheeproles_type)[agrep("Farmer",levels(farmingexp_df$sheeproles_type))]<-'Farmer/Stockperson/Vet'
-levels(farmingexp_df$sheeproles_type)[agrep("Stockman/woman/person",levels(farmingexp_df$sheeproles_type))]<-'Farmer/Stockperson/Vet'
-levels(farmingexp_df$sheeproles_type)[agrep("Veterinarian",levels(farmingexp_df$sheeproles_type))]<-'Farmer/Stockperson/Vet'
-levels(farmingexp_df$sheeproles_type)[levels(farmingexp_df$sheeproles_type)%in%("Other;")]<-'Other'
-levels(farmingexp_df$sheeproles_type)
-
-#reorder levels of lameness prevalence
-farmingexp_df$sheeplamenessexp_prev<-factor(farmingexp_df$sheeplamenessexp_prev, levels=c("Under 2%", "Between 2 and 5%", "Between 5 and 10%","Over 10%"))
-#combine lameness prevs cos poor sampling at tails
-levels(farmingexp_df$sheeplamenessexp_prev)[levels(farmingexp_df$sheeplamenessexp_prev)%in%c("Under 2%", "Between 2 and 5%")]<-"Under 5%"
-levels(farmingexp_df$sheeplamenessexp_prev)[levels(farmingexp_df$sheeplamenessexp_prev)%in%c("Between 5 and 10%","Over 10%")]<-"Over 5%"
-
-#reorder levels of lameness prevalence
-studydata_formatted$sheeplamenessexp_prev<-factor(studydata_formatted$sheeplamenessexp_prev, levels=c("Under 2%", "Between 2 and 5%", "Between 5 and 10%","Over 10%"))
-#combine lameness prevs cos poor sampling at tails
-#levels(studydata_formatted$sheeplamenessexp_prev)[levels(studydata_formatted$sheeplamenessexp_prev)%in%c("Under 2%", "Between 2 and 5%")]<-"Under 5%"
-#levels(studydata_formatted$sheeplamenessexp_prev)[levels(studydata_formatted$sheeplamenessexp_prev)%in%c("Between 5 and 10%","Over 10%")]<-"Over 5%"
-
-#remove not needed dataframe
-rm(farmingexp_df)
-
-# PARSE SHEEP ROLES -------------------------------------------------------------
-
+#make a vector of possible roles in which people may have worked with sheep
 roles<-c(
   'Farmer',
   'Stockman/woman/person',
   'Veterinarian',
   'Other')
 
+#create an empty dataframe with 63 rows (participants) and 4 cols (roles) 
 roles_df<-data.frame(matrix(nrow=nrow(studydata_formatted), ncol=length(roles)))
+#set col names to roles
 colnames(roles_df)<-roles
+#loop over each role and create a binary vector (column) that expresses whether or not the participant had worked in that role
 for (r in 1:length(roles)){
-  print(r)
+  #use grep to make a temporary vector to say whether or not (TRUE/FALSE) the role appeared in each participants' answer string (roles are ; seperated)
   temp<-grepl(roles[r],studydata_formatted$sheeproles_type)
+  #write this temporary vector to the column for the current role
   roles_df[,r]<-temp
 }
 
-#calculate any sheep role column
+#add a new column, 'Any', which expresses whether or not the participant had worked with sheep in any role
 roles_df<-cbind(roles_df,Any=rowSums(roles_df)>=1)
 
-#make row names participant IDs
+#make row names participant IDs (participant IDs start at 8, not 1)
 rownames(roles_df)<-studydata_formatted$ID
-#change colnames 
+#change colnames to the questiontype_subtype format 
 colnames(roles_df)<-c('sheeproles_farmer','sheeproles_stockperson','sheeproles_vet', 'sheeproles_other', 'sheeproles_any')
 
-#write csv with row names
+#write csv and RDS with row names
 write.csv(roles_df, 'outputs/processed_data/roles_df.csv', row.names = T)
 saveRDS(roles_df, 'outputs/processed_data/roles_df.RDS')
 
 #add to main dataframe
 studydata_formatted<-cbind(studydata_formatted,roles_df)
 
-#remove the original symptoms_lookedfor column to remove redundancy in the dataframe
+#remove the original sheeproles_type coluumn from the dataframe to remove redundancy in the dataframe
 studydata_formatted<-studydata_formatted[,-which(colnames(studydata_formatted)=='sheeproles_type')]
+
+#reorder levels of lameness prevalence so that they plot/model in order of increasing prevalence
+studydata_formatted$sheeplamenessexp_prev<-factor(studydata_formatted$sheeplamenessexp_prev, levels=c("Under 2%", "Between 2 and 5%", "Between 5 and 10%","Over 10%"))
 
 # PARSE LAMENESS SIGNS (SYMPTOMS) -------------------------------------------------------------
 
+#create a vector of symptoms
 symptoms<-c(
   'Uneven posture',
   'Shortened stride on one leg when walking',
@@ -123,34 +100,37 @@ symptoms<-c(
   'Slower walking pace',
   'Other')
 
+#create an empty dataframe with 63 rows (participants) and 9 cols (symptoms) 
 symptomslookedfor_df<-data.frame(matrix(nrow=nrow(studydata_formatted), ncol=length(symptoms)))
+#set col names to symptoms
 colnames(symptomslookedfor_df)<-symptoms
+#loop over each symptom and create a binary vector (column) that expresses whether or not the participant looked for that symptom
 for (s in 1:length(symptoms)){
-  print(s)
+  #use grep to make a temporary vector to say whether or not (TRUE/FALSE) the symptom appeared in each participants' answer string (symptoms are ; seperated)
   temp<-grepl(symptoms[s],studydata_formatted$symptoms_lookedfor)  
+  #write this temporary vector to the column for the current symptom
   symptomslookedfor_df[,s]<-temp
 }
 
-symptomslookedfor_df
-
+#shorten the names of some symptoms so that fit on plot x-axis labels easier
 colnames(symptomslookedfor_df)[which(colnames(symptomslookedfor_df)%in%c("Pair of legs which were moving at different speeds","Not weight bearing on affected leg when standing","Not weight bearing on affected leg when walking"))]<-c("Pair of legs moving at different speeds","Not weight bearing on affected leg (standing)", "Not weight bearing on affected leg (walking)")
+#do same for symptoms vector
 symptoms[which(symptoms%in%c("Pair of legs which were moving at different speeds","Not weight bearing on affected leg when standing","Not weight bearing on affected leg when walking"))]<-c("Pair of legs moving at different speeds","Not weight bearing on affected leg (standing)", "Not weight bearing on affected leg (walking)")
 
-#make row names participant IDs
+#make row names participant IDs (participant IDs start at 8, not 1)
 rownames(symptomslookedfor_df)<-studydata_formatted$ID
 
 #write symptoms looked for df (easier to use for plotting)
 saveRDS(symptomslookedfor_df,'outputs/processed_data/symptomslookedfor_df.RDS')
 write.csv(symptomslookedfor_df,'outputs/processed_data/symptomslookedfor_df.csv', row.names = T)
 
-#change colnames to shorthand
+#change colnames of dataframe to shorthand (we'll use the symptoms vector to label)
 colnames(symptomslookedfor_df)<-c('UP','SS','LS','NH','WBS','WBW','RM','SW','O')
 #add to main dataframe
 studydata_formatted<-cbind(studydata_formatted,symptomslookedfor_df)
 
 #remove the original symptoms_lookedfor column to remove redundancy in the dataframe
 studydata_formatted<-studydata_formatted[,-which(colnames(studydata_formatted)=='symptoms_lookedfor')]
-studydata_formatted$O
 
 # PARSE USER ENGAGEMENT ---------------------------------------------------
 
@@ -160,27 +140,34 @@ studydata_formatted$timespent<-(600-studydata_formatted$timeremaining_s)/60
 studydata_formatted<-studydata_formatted[,-which(colnames(studydata_formatted)=='timeremaining_s')]
 
 #change cpu setup levels
+#those strings containing 'mouse' become 'mouse'
 studydata_formatted$cpu_setup[agrep('Mouse',studydata_formatted$cpu_setup)]<-'Mouse'
+#those strings containing 'trackpad' become 'trackpad'
 studydata_formatted$cpu_setup[agrep('Track-pad',studydata_formatted$cpu_setup)]<-'Trackpad'
+#those remaining (i.e. only containing laptop or desktop computer) get 'unknown'
 studydata_formatted$cpu_setup[agrep('Laptop or Desktop computer;',studydata_formatted$cpu_setup)]<-'Unknown'
+#reorder levels for plotting
 studydata_formatted$cpu_setup<-factor(studydata_formatted$cpu_setup, levels=c('Unknown','Trackpad','Mouse'))
 
-#change columns to factor
+#change all columns to factor
 studydata_formatted$tutorial<-as.factor(studydata_formatted$tutorial)
-studydata_formatted$cpu_setup<-studydata_formatted$cpu_setup
+studydata_formatted$cpu_setup<-as.factor(studydata_formatted$cpu_setup)
 studydata_formatted$controlsprobs_YN<-as.factor(studydata_formatted$controlsprobs_YN)
 studydata_formatted$strategy_type<-as.factor(studydata_formatted$strategy_type)
 studydata_formatted$moving_type<-as.factor(studydata_formatted$moving_type)
 
-#edit levels
+#shorten level names for strategy type
 levels(studydata_formatted$strategy_type)<-c('Up-close','Zoomer','Other')
+#shorten level names for moving type
 levels(studydata_formatted$moving_type)<-c('Other','Randomly','Semi-randomly','Linear')
+#reorder levels for moving type for plotting
 studydata_formatted$moving_type<-factor(studydata_formatted$moving_type, levels=c('Linear','Semi-randomly','Randomly','Other'))
 
 # MAKE CONTINGENCY TABLE OF LAMENESS SIGNS LOOKED FOR BY FARMING EXPERIENCE -----------------------------------------------------------------------
 
+#count up the number of symptoms (row sums of symptoms looked for) by farming experience (create contigency table)
 symptomslookedfor_byfarmingexp_sum<-t(aggregate(symptomslookedfor_df, list(studydata_formatted$farmingexp_YN), function(x){sum(x)}))
-#put yes first for plotting purposes
+#put yes (farming experience) first for plotting purposes
 symptomslookedfor_byfarmingexp_sum<-symptomslookedfor_byfarmingexp_sum[,c(2,1)]
 #rename columns more clearly
 colnames(symptomslookedfor_byfarmingexp_sum)<-c('Farming experience','No farming experience')
