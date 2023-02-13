4249c4993ba836ecc4ab68e8e4cbff9348fec7d6
Matt Lloyd Jones <42059798+befriendabacterium@users.noreply.github.com>
2023-02-13 11:21:34 +0000

    Remove mention of data_folder
    Redundant relict of old workflow

--- src/1_downloaddata.R
+++ src/1_downloaddata.R
@@ -6,5 +6,5 @@ my_project <- osfr::osf_ls_files(osfr::osf_retrieve_node("a6qu4"))
 #download all the folders (inputs and outputs)
 osfr::osf_download(my_project,getwd(), recurse = T, conflicts='overwrite')
 #remove the osf project as no longer needed as we have a local copy
-rm(my_project, data_folder)
+rm(my_project)
 
